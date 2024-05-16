a2b133da4de4a83b56d822b6c6781a1aaf0f4b97
Maria Zadnepryanets <mariya.z@gmail.com>
2024-05-16 11:19:35 +0200

    part1: excercise 1.6 - add buttons, headers, stats

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Header = ({ text }) => <h1>{text}</h1>
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+const Statistics = ({ text, value }) => <p>{text} {value}</p>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,9 +15,18 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <Header text="give feedback" />
+      <div>
+        <Button onClick={() => setGood(good + 1)} text="good" />
+        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button onClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+      <Header text="statistics" />
+        <Statistics text="good" value={good} />
+        <Statistics text="neutral" value={neutral} />
+        <Statistics text="bad" value={bad} />
+    </>
   )
 }
 
