1bdfb2043a7bf8ee27a91d34e00bbd1bd1b6ffb6
Maria Zadnepryanets <mariya.z@gmail.com>
2024-05-16 11:41:11 +0200

    part1: ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,26 @@ const Button = ({ onClick, text }) => (
     {text}
   </button>
 )
-const Statistics = ({ text, value }) => <p>{text} {value}</p>
+const Stat = ({ text, value }) => <p>{text} {value}</p>
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all // good: 1, neutral: 0, bad: -1
+  const positive = (good / all) * 100
+  
+  return (
+    <>
+      <Header text="statistics" />
+      <>
+        <Stat text="good" value={good} />
+        <Stat text="neutral" value={neutral} />
+        <Stat text="bad" value={bad} />
+        <Stat text="all" value={all} />
+        <Stat text="average" value={average ? average : 0} />
+        <Stat text="positive" value={`${positive ? positive : 0} %`} />
+      </>
+    </>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -14,11 +33,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const handleClick = (handler, value) => () => handler(value)
-  // Statistics calculation
-  const all = good + neutral + bad
-  const average = (good - bad) / all // good: 1, neutral: 0, bad: -1
-  const positive = (good / all) * 100
-
+ 
   return (
     <>
       <Header text="give feedback" />
@@ -27,13 +42,7 @@ const App = () => {
         <Button onClick={handleClick(setNeutral, neutral + 1)} text="neutral" />
         <Button onClick={handleClick(setBad, bad + 1)} text="bad" />
       </div>
-      <Header text="statistics" />
-        <Statistics text="good" value={good} />
-        <Statistics text="neutral" value={neutral} />
-        <Statistics text="bad" value={bad} />
-        <Statistics text="all" value={all} />
-        <Statistics text="average" value={average ? average : 0} />
-        <Statistics text="positive" value={`${positive ? positive : 0} %`} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
