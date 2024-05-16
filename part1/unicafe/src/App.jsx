0608c607ee3cfc9f3f45d7b2399b40676eefc6a1
Maria Zadnepryanets <mariya.z@gmail.com>
2024-05-16 11:23:24 +0200

    part1: ex 1.6 - refactoring

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,18 +9,19 @@ const Button = ({ onClick, text }) => (
 const Statistics = ({ text, value }) => <p>{text} {value}</p>
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const handleClick = (handler, value) => () => handler(value)
 
   return (
     <>
       <Header text="give feedback" />
       <div>
-        <Button onClick={() => setGood(good + 1)} text="good" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button onClick={() => setBad(bad + 1)} text="bad" />
+        <Button onClick={handleClick(setGood, good + 1)} text="good" />
+        <Button onClick={handleClick(setNeutral, neutral + 1)} text="neutral" />
+        <Button onClick={handleClick(setBad, bad + 1)} text="bad" />
       </div>
       <Header text="statistics" />
         <Statistics text="good" value={good} />
