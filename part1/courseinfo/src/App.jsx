faf2e6249de186d7cdcee10fc4abe70951db4d80
Maria Zadnepryanets <mariya.z@gmail.com>
2024-05-16 11:24:49 +0200

    part1: ex1.5 - refactoring

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,15 +1,15 @@
-const Header = ({course}) => <h1>{course}</h1>
-const Part = ({name, exercises}) => <p>{name} {exercises}</p>
-const Content = ({parts}) => parts.map(({name, exercises}) => <Part key={name} name={name} exercises={exercises} />)
-const Total = ({parts}) => <p>Number of exercises {parts.reduce((acc, part) => acc + part["exercises"], 0)}</p>
+const Header = ({ course }) => <h1>{course}</h1>
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+const Content = ({ parts }) => parts.map(({ name, exercises }) => <Part key={name} name={name} exercises={exercises} />)
+const Total = ({ parts }) => <p>Number of exercises {parts.reduce((acc, part) => acc + part["exercises"], 0)}</p>
 
 function App() {
   const course = {
     name: 'Half Stack application development',
     parts: [
-      {name: 'Fundamentals of React', exercises: 10},
-      {name: 'Using props to pass data', exercises: 7},
-      {name: 'State of a component', exercises: 14}
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
     ]
   }
 
