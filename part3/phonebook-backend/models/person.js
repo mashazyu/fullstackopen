d2127c3bd81d15c665e37640794f061b3a9233e1
Maria Zadnepryanets <mariya.z@gmail.com>
2024-07-31 13:19:35 +0200

    part3: fix phone number validation on backend

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -27,7 +27,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: function (v) {
-        return /\d{2}-\d\^[^-]+$/.test(v);
+        return /^(?:\d{2}-\d{5,}|\d{3}-\d{5,})$/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
