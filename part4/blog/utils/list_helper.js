6c653519bdd941b038c9ec93918a8309c543d3bb
Maria Zadnepryanets <mariya.z@gmail.com>
2024-09-30 18:51:50 +0200

    part4: ex 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,11 +2,30 @@ const dummy = (blogs) => {
     return 1
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return {}
+
+    let favorite = {...blogs[0]}
+
+    blogs.forEach(blog => {
+        if (blog.likes > favorite.likes) {
+            favorite = {...blog}
+        }
+    })
+
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
 const totalLikes = (blogs) => {
     return blogs.reduce((acc, val) => acc + val.likes, 0)
 }
   
 module.exports = {
     dummy,
+    favoriteBlog,
     totalLikes
 }
