62fbb9c1549d81a4e59eaf164675ec8bc03bfb1b
Maria Zadnepryanets <mariya.z@gmail.com>
2024-09-30 14:26:12 +0200

    part4: ex 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((acc, val) => acc + val.likes, 0)
+}
   
-  module.exports = {
-    dummy
-  }
+module.exports = {
+    dummy,
+    totalLikes
+}
