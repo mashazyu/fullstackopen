cd04937bba90c04f202ad33f830b0553a621d243
Maria Zadnepryanets <mariya.z@gmail.com>
2024-10-15 11:45:38 +0200

    part4: install prettier and fix linter errors

--- part4/blog/tests/blogs_api.test.js
+++ part4/blog/tests/blogs_api.test.js
@@ -1,59 +1,59 @@
-const { test, after, beforeEach } = require("node:test");
-const assert = require("node:assert");
-const mongoose = require("mongoose");
-const supertest = require("supertest");
-const app = require("../app");
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
 
-const api = supertest(app);
+const api = supertest(app)
 
-const Blog = require("../models/blog");
+const Blog = require('../models/blog')
 
 const initialBlogs = [
   {
-    title: "title",
-    author: "famous author",
-    url: "http://website1.com",
+    title: 'title',
+    author: 'famous author',
+    url: 'http://website1.com',
     likes: 2,
-    id: "66fa8dfbe75e597b07c68a29",
+    id: '66fa8dfbe75e597b07c68a29',
   },
   {
-    title: "longer title",
-    author: "very famous author",
-    url: "http://website2.com",
+    title: 'longer title',
+    author: 'very famous author',
+    url: 'http://website2.com',
     likes: 22,
-    id: "66fa9246fbc8a71ff5ff5f4d",
+    id: '66fa9246fbc8a71ff5ff5f4d',
   },
-];
+]
 
 beforeEach(async () => {
-  await Blog.deleteMany({});
+  await Blog.deleteMany({})
 
-  const blogObjects = initialBlogs.map((blog) => new Blog(blog));
-  const promiseArray = blogObjects.map((blog) => blog.save());
+  const blogObjects = initialBlogs.map((blog) => new Blog(blog))
+  const promiseArray = blogObjects.map((blog) => blog.save())
 
-  await Promise.all(promiseArray);
-});
+  await Promise.all(promiseArray)
+})
 
-test("blogs are returned as json", async () => {
+test('blogs are returned as json', async () => {
   await api
-    .get("/api/blogs")
+    .get('/api/blogs')
     .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+    .expect('Content-Type', /application\/json/)
+})
 
-test("there are two blog posts", async () => {
-  const response = await api.get("/api/blogs");
+test('there are two blog posts', async () => {
+  const response = await api.get('/api/blogs')
 
-  assert.strictEqual(response.body.length, 2);
-});
+  assert.strictEqual(response.body.length, 2)
+})
 
-test("the first blog has correct properties", async () => {
-  const response = await api.get("/api/blogs");
-  const contents = response.body.map((e) => e.content);
+test('the first blog has correct properties', async () => {
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map((e) => e.content)
 
-  assert.strictEqual(contents.includes("HTML is easy"), true);
-});
+  assert.strictEqual(contents.includes('HTML is easy'), true)
+})
 
 after(async () => {
-  await mongoose.connection.close();
-});
+  await mongoose.connection.close()
+})
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,73 +1,73 @@
-const dummy = (blogs) => {
-    return 1
+const dummy = () => {
+  return 1
 }
 
 const favoriteBlog = (blogs) => {
-    if (blogs.length === 0) return {}
+  if (blogs.length === 0) return {}
 
-    let favorite = {...blogs[0]}
+  let favorite = {...blogs[0]}
 
-    blogs.forEach(blog => {
-        if (blog.likes > favorite.likes) {
-            favorite = {...blog}
-        }
-    })
-
-    return {
-        title: favorite.title,
-        author: favorite.author,
-        likes: favorite.likes
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes) {
+      favorite = {...blog}
     }
+  })
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
 }
 
 const groupedBlogs = (blogs) => blogs.reduce((acc, curr) => {
-    const groupKey = curr["author"];
+  const groupKey = curr['author']
 
-    if (!acc[groupKey]) {
-        acc[groupKey] = [];
-    }
+  if (!acc[groupKey]) {
+    acc[groupKey] = []
+  }
 
-    acc[groupKey].push(curr);
+  acc[groupKey].push(curr)
 
-    return acc;
+  return acc
 }, {})
 
 const mostBlogs = (blogs) => {
-    if (blogs.length === 0) return {}
+  if (blogs.length === 0) return {}
 
-    const grouped = groupedBlogs(blogs)
-    const authorsWithBlogCount = Object.keys(grouped)
-        .map(key => ({ "author": key, "blogs": grouped[key].length })) 
+  const grouped = groupedBlogs(blogs)
+  const authorsWithBlogCount = Object.keys(grouped)
+    .map(key => ({ 'author': key, 'blogs': grouped[key].length })) 
     
-    return authorsWithBlogCount.reduce((prev, curr) => {
-        return (prev.blogs > curr.blogs) ? prev : curr
-    })
+  return authorsWithBlogCount.reduce((prev, curr) => {
+    return (prev.blogs > curr.blogs) ? prev : curr
+  })
 }
 
 const mostLikes = (blogs) => {
-    if (blogs.length === 0) return {}
+  if (blogs.length === 0) return {}
 
-    const grouped = groupedBlogs(blogs)
-    const authorsWithLikesCount = Object.keys(grouped)
-        .map(key => {
-            const likes = grouped[key].reduce((acc, curr) => (acc + curr.likes), 0)
+  const grouped = groupedBlogs(blogs)
+  const authorsWithLikesCount = Object.keys(grouped)
+    .map(key => {
+      const likes = grouped[key].reduce((acc, curr) => (acc + curr.likes), 0)
 
-            return ({ "author": key, likes })
-        }) 
+      return ({ 'author': key, likes })
+    }) 
     
-    return authorsWithLikesCount.reduce((prev, curr) => {
-        return (prev.likes > curr.likes) ? prev : curr
-    })
+  return authorsWithLikesCount.reduce((prev, curr) => {
+    return (prev.likes > curr.likes) ? prev : curr
+  })
 }
 
 const totalLikes = (blogs) => {
-    return blogs.reduce((acc, val) => acc + val.likes, 0)
+  return blogs.reduce((acc, val) => acc + val.likes, 0)
 }
   
 module.exports = {
-    dummy,
-    favoriteBlog,
-    mostBlogs,
-    mostLikes,
-    totalLikes
+  dummy,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+  totalLikes
 }
