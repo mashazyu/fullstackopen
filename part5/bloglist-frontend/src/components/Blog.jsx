b96cb41eaf6f6a8cb0e644fc8738eeab1842d012
Maria Zadnepryanets <mariya.z@gmail.com>
2025-01-30 14:54:11 +0100

    part5: ex5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useRef, useState, useEffect } from 'react'
+
+const Blog = ({ blog }) => {
+  const [fullDetails, setFullDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const buttonLabel = fullDetails ? "hide" : "view"
+  const handleDetailsVisibility = () => setFullDetails(!fullDetails)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <div>{blog.title} {blog.author} <button onClick={handleDetailsVisibility}>{buttonLabel}</button></div>
+        {fullDetails && (
+          <div> 
+            {blog.url}
+            <div>likes {blog.likes} <button>like</button></div>
+            {blog.user?.username}
+          </div>
+        )}
+      </div>
+  </div>
+)}
 
 export default Blog
