affde5e22f40bc696bc63608aa541bb925b620be
Maria Zadnepryanets <mariya.z@gmail.com>
2025-01-31 12:51:06 +0100

    part5: ex5.17

---
+++ part5/bloglist-frontend/tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Blog app', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('login to application')).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: /login/i })).toBeVisible()
+  })
+})
