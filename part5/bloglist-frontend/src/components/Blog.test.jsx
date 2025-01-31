b90287267768005efc85b2f9a859f2a5385aa9d4
Maria Zadnepryanets <mariya.z@gmail.com>
2025-01-31 11:14:54 +0100

    part5: ex5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,30 +1,48 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    id: 'id',
-    title: 'Component testing is done with react-testing-library',
-    author: 'author',
-    url: 'https://nonexistingdomain.com',
-    user: {
-      username: 'currentuser',
-      id: 'userid'
-    }
-  }
-  const user = {
+const blog = {
+  id: 'id',
+  title: 'Component testing is done with react-testing-library',
+  author: 'author',
+  url: 'https://nonexistingdomain.com',
+  user: {
     username: 'currentuser',
-    token: 'token'
+    id: 'userid'
   }
+}
+const user = {
+  username: 'currentuser',
+  token: 'token'
+}
 
-  const container = render(< Blog blog={blog} user={user}/>)
+describe('<Togglable />', () => {
+  let container
 
-  const title = screen.getByText('Component testing is done with react-testing-library', { exact: false })
-  expect(title).toBeDefined()
-  const author = screen.getByText('author', { exact: false })
-  expect(author).toBeDefined()
-  const likes = container.queryByTestId('likes')
-  expect(likes).toBeFalsy()
-  const url = container.queryByTestId('url', { exact: false })
-  expect(url).toBeFalsy()
+  beforeEach(() => {
+    container = render(< Blog blog={blog} user={user}/>)
+  })
+
+  test('renders title and author by default', () => {
+    screen.getByText('Component testing is done with react-testing-library', { exact: false })
+    screen.getByText('author', { exact: false })
+  })
+
+  test('does not render likes and url by default', () => {
+    const likes = container.queryByTestId('likes')
+    expect(likes).toBeFalsy()
+    const url = container.queryByTestId('url')
+    expect(url).toBeFalsy()
+  })
+
+  test('renders url and likes after hitting view button', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    container.getByTestId('likes')
+    container.getByTestId('url')
+  })
 })
